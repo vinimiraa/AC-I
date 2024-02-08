9a0a542dee11c37fa91301716ef3fde816c884de
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-02-08 18:32:30 -0300

    Update Guia_01

--- Guia_01/Guia_01.java
+++ Guia_01/Guia_01.java
@@ -14,8 +14,9 @@ public class Guia_01
      *  @param x - primeiro valor
      *  @param y - segundo valor
      */
-    public static void test_equals(Object x, Object y) {
-        if (("" + x).compareTo("" + y) != 0)
+    public static void test_equals( Object x, Object y ) 
+    {
+        if ( ( "" + x ).compareTo( "" + y ) != 0 )
             errors = errors + 1;
     } // end test_equals ( )
 
@@ -23,8 +24,9 @@ public class Guia_01
      *  Exibir o total de erros.
      *  @return mensagem com o total de erros
      */
-    public static String test_report() {
-        return ("" + errors);
+    public static String test_report( ) 
+    {
+        return ( "" + errors );
     } // end test_report ( )
 
     /**
@@ -32,7 +34,8 @@ public class Guia_01
      *  @return binario equivalente
      *  @param value - valor decimal
      */
-    public static String dec2bin(int value) {
+    public static String dec2bin( int value ) 
+    {
         int binary = 0;
         int bit = 0;
         int position = 1;
@@ -45,7 +48,7 @@ public class Guia_01
             position = position * 10;
         } // end while
 
-        return ( Integer.toString(binary) );
+        return ( Integer.toString( binary ) );
     } // end dec2bin ( )
 
     /**
@@ -53,7 +56,7 @@ public class Guia_01
      *  @return decimal equivalente
      *  @param value - valor binario
      */
-    public static int bin2dec(String value) 
+    public static int bin2dec( String value ) 
     {
         int length = 0;
         int decimal = 0;
@@ -66,7 +69,7 @@ public class Guia_01
             decimal = decimal + bit * (int)Math.pow(2, length-1-x);
         } // end for
 
-        return (decimal);
+        return ( decimal );
     } // end bin2dec ( )
 
     /**
@@ -74,7 +77,7 @@ public class Guia_01
      *  @return base para a conversao
      *  @param value - valor decimal
      */
-    public static String dec2base(int value, int base) 
+    public static String dec2base( int value, int base ) 
     {
         // int result = 0;
         int bit = 0;
@@ -89,19 +92,19 @@ public class Guia_01
             {
                 digit = (char)( 'A' + ( bit - 10 ) );
                 // result = result + ( digit - '0' ) * position;
-                result.insert(0, digit);
+                result.insert( 0, digit );
             }
             else
             {
                 // result = result + bit * position;
-                result.insert(0, bit);
+                result.insert( 0, bit );
             } // end if
             value = value / base;
             position = position * 10;
         } // end for
 
         // return ( Integer.toString(result) );
-        return ( result.toString() );
+        return ( result.toString( ) );
     } // end dec2base ( )
 
     /**
@@ -110,9 +113,9 @@ public class Guia_01
      *  @param value - valor binario
      *  @param base - para a conversao
      */
-    public static String bin2base(String value, int base) 
+    public static String bin2base( String value, int base ) 
     {
-        return ( dec2base( bin2dec(value) , base) );
+        return ( dec2base( bin2dec( value ) , base ) );
     } // end bin2base ( )
 
     /**
@@ -120,22 +123,22 @@ public class Guia_01
      *  @return hexadecimal equivalente
      *  @param value - caractere(s) em codigo ASCII
      */
-    public static String ASCII2hex(String value) 
+    public static String ASCII2hex( String value ) 
     {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder( );
         int ascii = 0;
         String hex;
 
-        for ( int x = 0; x < value.length(); x = x + 1 )
+        for ( int x = 0; x < value.length() ; x = x + 1 )
         {
-            ascii = (int) value.charAt(x);
+            ascii = (int) value.charAt( x );
 
             hex = dec2base(ascii, 16);
 
-            result.append(hex).append(" ");
+            result.append( hex ).append( " " );
             // result.insert(0, dec2base( (int)value.charAt(x) , 16));
         }
-        return ( result.toString() );
+        return ( result.toString( ) );
     } // end ASCII2hex ( )
 
     /**
@@ -143,7 +146,8 @@ public class Guia_01
      *  @return caractere(s) em codigo ASCII
      *  @param value - hexadecimal equivalente(s)
      */
-    public static String hex2ASCII(String value) {
+    public static String hex2ASCII( String value ) 
+    {
         return ("0");
     } // end hex2ASCII ( )
 
@@ -152,55 +156,51 @@ public class Guia_01
      */
     public static void main( String[] args ) 
     {
-        System.out.println("Guia_01 - Java Tests ");
-        System.out.println("812839 - Vinicius Miranda de Araujo ");
-        System.out.println();
+        System.out.println( "Guia_01 - Java Tests "                );
+        System.out.println( "812839 - Vinicius Miranda de Araujo " );
+        System.out.println( );
 
-        System.out.println("dec2base( bin2dec( 10111 ), 4) " + dec2base( bin2dec( "10111" ), 4));
-
-        System.err.println( "ASCII2hex( PUC-M.G.) = " + ASCII2hex("PUC-M.G.") );
-
-        test_equals(dec2bin(23), "10111");
-        test_equals(dec2bin(57), "111001");
-        test_equals(dec2bin(732), "1011011100");
-        test_equals(dec2bin(321), "101000001");
-        test_equals(dec2bin(364), "101101100");
+        test_equals( dec2bin( 23  ), "10111"      );
+        test_equals( dec2bin( 57  ), "111001"     );
+        test_equals( dec2bin( 732 ), "1011011100" );
+        test_equals( dec2bin( 321 ), "101000001"  );
+        test_equals( dec2bin( 364 ), "101101100"  );
         System.out.println("1. errorTotalReport = " + test_report());
 
-        test_equals(bin2dec("10011"), 19);
-        test_equals(bin2dec("11101"), 29);
-        test_equals(bin2dec("10110"), 22);
-        test_equals(bin2dec("101101"), 45);
-        test_equals(bin2dec("110011"), 51);
+        test_equals( bin2dec( "10011"  ), 19 );
+        test_equals( bin2dec( "11101"  ), 29 );
+        test_equals( bin2dec( "10110"  ), 22 );
+        test_equals( bin2dec( "101101" ), 45 );
+        test_equals( bin2dec( "110011" ), 51 );
         System.out.println("2. errorTotalReport = " + test_report());
         
-        test_equals(dec2base(67, 4), "1003");
-        test_equals(dec2base(58, 8), "72");
-        test_equals(dec2base(76, 16), "4C");
-        test_equals(dec2base(157, 16), "9D");
-        test_equals(dec2base(735, 16), "2DF");
+        test_equals( dec2base( 67 , 4  ), "1003" );
+        test_equals( dec2base( 58 , 8  ), "72"   );
+        test_equals( dec2base( 76 , 16 ), "4C"   );
+        test_equals( dec2base( 157, 16 ), "9D"   );
+        test_equals( dec2base( 735, 16 ), "2DF"  );
         System.out.println("3. errorTotalReport = " + test_report());
 
-        test_equals(bin2base("10111", 4), "113");
-        test_equals(bin2base("11110", 8), "36");
-        test_equals(bin2base("100101", 16), "25");
-        test_equals(bin2base("101011", 8), "53");
-        test_equals(bin2base("101100", 4), "230");
+        test_equals( bin2base( "10111" , 4  ), "113" );
+        test_equals( bin2base( "11110" , 8  ), "36"  );
+        test_equals( bin2base( "100101", 16 ), "25"  );
+        test_equals( bin2base( "101011", 8  ), "53"  );
+        test_equals( bin2base( "101100", 4  ), "230" );
         System.out.println("4. errorTotalReport = " + test_report());
 
-        test_equals(ASCII2hex("PUC-M.G."), "50 55 43 2D 4D 2E 47 2E ");
-        test_equals(ASCII2hex("2024-01"), "32 30 32 34 2D 30 31 ");
-        // test_equals(ASCII2hex("Minas Gerais"), 
+        test_equals( ASCII2hex( "PUC-M.G." ), "50 55 43 2D 4D 2E 47 2E " );
+        test_equals( ASCII2hex( "2024-01"  ), "32 30 32 34 2D 30 31 "    );
+        // test_equals( ASCII2hex( "Minas Gerais" ), 
         // "01001101 01101001 01101110 01100001 01110011 00100000 01000111 01100101 01110010 01100001 01101001 0111001");
         // test_equals(hex2ASCII("116 117 111 124 105"), "NOITE"); // OBS.: 116 e' o primeiro octal (0116)!
-        test_equals(hex2ASCII("54 61 72 64 65"), "Tarde"); // OBS.: 54 e' o primeiro hexadecimal (0x54)!
-        System.out.println("5. errorTotalReport = " + test_report());
+        test_equals( hex2ASCII( "54 61 72 64 65" ), "Tarde" ); // OBS.: 54 e' o primeiro hexadecimal (0x54)!
+        System.out.println( "5. errorTotalReport = " + test_report( ) );
 
-        System.out.print("\n\nApertar ENTER para terminar.");
+        System.out.print( "\n\nApertar ENTER para terminar." );
         try {
-            System.console().readLine();
+            System.console( ).readLine( );
             
-        } catch (Exception e) {
+        } catch ( Exception e ) {
             System.err.println( "Erro: leitura invalida." );
         }
     } // end main
