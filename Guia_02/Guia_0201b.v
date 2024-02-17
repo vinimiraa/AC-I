2d1536e7a4ff3b27951ba22d865ee5d71a5892d8
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-02-17 12:19:56 -0300

    Update Guia_02

--- Guia_02/Guia_0201b.v
+++ Guia_02/Guia_0201b.v
@@ -5,26 +5,32 @@
 
 module Guia_0201; 
 
-   real            x =   0 ; // decimal 
-   real  power2 = 1.0; // power of 2 
-   integer       y =   7 ; // counter 
-   reg [7:0]     b = 8'b10100000; // binary (only fraction part, Big Endian) 
+   integer x = 0 ;     // var
+   integer y = 7 ;     // counter 
+   real power2 = 1.0 ; // power of 2 
+   real dec [0:4] ;    // decimal 
+   reg  [0:4][7:0] bin = {8'b00101000, 8'b01011000, 8'b10001000, 8'b11011000, 8'b10011000}; // binary (only fraction part, Big Endian)
 
    initial begin : main 
    
-      $display ( "Guia_0201 - Tests" ); 
-      $display ( "x = %f" , x ); 
-      $display ( "b = 0.%8b", b ); 
-      while ( y >= 0 ) 
-         begin 
-         power2 = power2 / 2.0; 
-         if ( b[y] == 1 ) 
-         begin 
-            x = x + power2; 
-         end 
-         $display ( "x = %f", x ); 
-         y=y-1; 
-         end // end while 
+      $display ( "Guia_0201 - Tests" );  
+
+      for ( x = 0; x < 5 ; x = x + 1 ) begin
+         y = 7;
+         while ( y >= 0 ) begin 
+            power2 = power2 / 2.0; 
+
+            if ( bin[x][y] == 1 ) 
+            begin 
+               temp_dec = temp_dec + power2; 
+            end // end if
+            y=y-1; 
+         end // end while
+      end // end for
+
+      for ( x = 0; x < 5; x = x + 1 ) begin
+         $display ( "%d = %f",x+1, dec[x] ); 
+      end // end for
 
    end // main 
 
--- Guia_02/Guia_0204b.v
+++ Guia_02/Guia_0204b.v
@@ -5,23 +5,28 @@
 
 module Guia_0204; 
   
-  real      x = 0.625;          // decimal 
-  reg [7:0] b = 8'b1010_0000  ; // binary 
-  integer q [3:0]; 
-  
+  reg [7:0]  a = 8'd213  ; // base-4
+  reg [10:0] b = 16'h4a2 ; // hexadecimal 
+  reg [8:0]  c = 12'o747 ; // octal 
+  reg [9:0]  d = 12'd654 ; // base-4
+  reg [11:0] e = 16'ha51 ; // hexadecimal 
+  reg [15:0] bin = 16'b0;   
+
   initial begin : main 
 
-    $display ( "Guia_0204 - Tests" ); 
-    $display ( "x = %f" , x ); 
-    $display ( "b = 0.%8b", b ); 
-    $display ( "b = 0.%x%x (16)", b[7:4],b[3:0] ); 
-    q[3] = b[7:6]; 
-    q[2] = b[5:4]; 
-    q[1] = b[3:2]; 
-    q[0] = b[1:0]; 
-    $display ( "b = 0.%2b %2b %2b %2b (2)", b[7:6],b[5:4],b[3:2],b[1:0] ); 
-    $display ( "q = 0.%2d %2d %2d %2d (4)", q[3]  ,q[2]  ,q[1]  ,q[0]   ); 
+    $display ( "Guia_0204 - Tests" );
+
+    bin = a;
+    $display ( "a = 0.%d (4) = 0.%b (2)" , a, bin[9:0]);
+    bin = b;
+    $display ( "b = 0.%x (16) = 0.%b (2)" , b , bin[9:0] );
+    bin = c;
+    $display ( "c = 0.%o (8) = 0.%b (2)" , c , bin[9:0] );
+    bin = d;
+    $display ( "d = 0.%d (4) = 0.%b (2) = 0.%d%d%d%d (4)" , d , bin[9:0], bin[7:6], bin[5:4], bin[3:2], bin[1:0] );
+    bin = e; 
+    $display ( "e = 0.%x (16) = 0.%b (2) = 0.%d%d%d%d (4)" , e , bin[9:0], bin[7:6], bin[5:4], bin[3:2], bin[1:0] );
     
   end // main 
 
-endmodule // Guia_0204 
+endmodule // Guia_0204
